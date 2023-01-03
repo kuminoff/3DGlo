adbb60a79749f979f2bfd45e1ad48557d11c25e1
Viacheslav Kuminov <kuminov200220@gmail.com>
2023-01-03 19:35:49 +0700

    12

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require("path");
 
 module.exports = {
-    context: path.resolve(__dirname, "src"),
-    entry: "./index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist/js")
-    },
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist/js"),
+  },
 
-    devServer: {
-        hot: true,
-        static: {
-            directory: "./dist/js",
-            watch: true
-        }
-    }
-}
+  devServer: {
+    hot: true,
+    static: {
+      directory: "./dist",
+      watch: true,
+    },
+  },
+};
