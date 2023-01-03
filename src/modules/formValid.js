f5332ae6e012dc50e24c05eab26eb194b85cc6a4
Viacheslav Kuminov <kuminov200220@gmail.com>
2023-01-03 17:52:41 +0700

    21_COMP v.2

--- src/modules/formValid.js
+++ src/modules/formValid.js
@@ -12,14 +12,14 @@ const formValid = () => {
   formItemsText.forEach((formItemText) => {
     formItemText.addEventListener("input", () => {
       console.log("Сообщение");
-      formItemText.value = formItemText.value.replace(/[^а-яА-Я\s\-]+/, ""); //
+      formItemText.value = formItemText.value.replace(/[^а-яА-Я\s\-]+/g, "");
     });
   });
 
   formItemsEmail.forEach((formItemEmail) => {
     formItemEmail.addEventListener("input", () => {
       formItemEmail.value = formItemEmail.value.replace(
-        /[^A-Za-z\d\@\-\_\.\!\~\*\']+/,
+        /[^a-zA-Z\d\@\-\_\.\!\~\*\']+/g,
         ""
       );
     });
@@ -27,7 +27,42 @@ const formValid = () => {
 
   formItemsTel.forEach((formItemTel) => {
     formItemTel.addEventListener("input", () => {
-      formItemTel.value = formItemTel.value.replace(/[^\d\\(\\)\-]+/, "");
+      formItemTel.value = formItemTel.value.replace(/[^\d\\(\\)\-]+/g, "");
+    });
+  });
+
+  formItemsText.forEach((formItemText) => {
+    formItemText.addEventListener("blur", () => {
+      console.log("Сообщение");
+      formItemText.value = formItemText.value.replace(/[\s]+/g, " ");
+      formItemText.value = formItemText.value.replace(/[\-]+/g, "-");
+      formItemText.value = formItemText.value.replace(/[\s\-]+/g, "-");
+      formItemText.value = formItemText.value.replace(/^[\s\-]+/g, "");
+      formItemText.value = formItemText.value.replace(/[\s\-]+$/g, "");
+      formItemText.value = formItemText.value.replace(
+        /( |^)[а-яА-Я]/g,
+        function (x) {
+          return x.toUpperCase();
+        }
+      );
+    });
+  });
+
+  formItemsEmail.forEach((formItemEmail) => {
+    formItemEmail.addEventListener("blur", () => {
+      formItemEmail.value = formItemEmail.value.replace(/[\-]+/g, "-");
+      formItemEmail.value = formItemEmail.value.replace(/[\s\-]+/g, "-");
+      formItemEmail.value = formItemEmail.value.replace(/^[\s\-]+/g, "");
+      formItemEmail.value = formItemEmail.value.replace(/[\s\-]+$/g, "");
+    });
+  });
+
+  formItemsTel.forEach((formItemTel) => {
+    formItemTel.addEventListener("blur", () => {
+      formItemTel.value = formItemTel.value.replace(/[\-]+/g, "-");
+      formItemTel.value = formItemTel.value.replace(/^[\s\-]+[\s\-]+/g, "");
+      formItemTel.value = formItemTel.value.replace(/[\s\-]+$/g, "");
+      formItemTel.value = formItemTel.value.replace(/[\s\-]+/g, "-");
     });
   });
 };
