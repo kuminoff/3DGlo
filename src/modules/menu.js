3ed42abbc5158c40a30f67d99134fae0997b1cf2
Viacheslav Kuminov <kuminov200220@gmail.com>
2023-01-03 17:31:10 +0700

    20_COMP v.1

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -7,6 +7,17 @@ const menu = () => {
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
   const menuItems = menu.querySelectorAll("ul>li>a");
+  const mouseScroll = document.querySelector("main>a");
+
+  const smoothScroll = (e, item) => {
+    e.preventDefault();
+    const blockID = item.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
 
   const handleMenu = () => {
     menu.classList.toggle("active-menu");
@@ -14,7 +25,16 @@ const menu = () => {
 
   menuBtn.addEventListener("click", handleMenu);
   closeBtn.addEventListener("click", handleMenu);
-  menuItems.forEach((item) => item.addEventListener("click", handleMenu));
+  menuItems.forEach((item) =>
+    item.addEventListener("click", (event) => {
+      handleMenu();
+      smoothScroll(event, item);
+    })
+  );
+
+  mouseScroll.addEventListener("click", (event) => {
+    smoothScroll(event, mouseScroll);
+  });
 };
 
 export default menu;
