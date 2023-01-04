68c44ac8005279b5bb323fff951b3c87ece8d2d6
Viacheslav Kuminov <kuminov200220@gmail.com>
2023-01-04 22:56:41 +0700

    20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -6,23 +6,27 @@ const modal = () => {
   const buttons = document.querySelectorAll(".popup-btn");
   const modal = document.querySelector(".popup");
   const closeBtn = modal.querySelector(".popup-close");
-  modal.style.transform = "translateY(50%)";
-  let count = 150;
+  modal.style.transform = "translateY(100%)";
+  let count = 50;
   let modalHandle;
 
   buttons.forEach((item) =>
     item.addEventListener("click", () => {
       modal.style.display = "block";
-      if (screen.width > 768) modalShow();
+      screen.width > 768 ? modalShow() : (modal.style.transform = "");
     })
   );
-  closeBtn.addEventListener("click", () => (modal.style.display = "none"));
+  closeBtn.addEventListener("click", () => {
+    count = 50;
+    modal.style.transform = "translateY(100%)";
+    modal.style.display = "none";
+  });
 
   const modalShow = () => {
     if (count > 0) {
       count--;
       modalHandle = requestAnimationFrame(modalShow);
-      modal.style.transform = `translateY(${-count * 4}%)`;
+      modal.style.transform = `translateY(${-count * 3}%)`;
     } else {
       cancelAnimationFrame(modalHandle);
     }
