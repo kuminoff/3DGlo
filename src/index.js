75460fd9b6a526113a2bff28c6e2cad09f50e731
Viacheslav Kuminov <kuminov200220@gmail.com>
2023-01-05 15:35:49 +0700

    22

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import calcValid from "./modules/calcValid.js";
 import formValid from "./modules/formValid.js";
 import tabs from "./modules/tabs";
 
-timer("25 january 2023");
+timer("14 january 2023");
 menu();
 modal();
 calcValid();
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,19 +1,25 @@
+/*jshint esversion: 6 */
+
 const timer = (deadline) => {
   const timerHours = document.getElementById("timer-hours");
   const timerMinutes = document.getElementById("timer-minutes");
   const timerSeconds = document.getElementById("timer-seconds");
+  const timerDays = document.getElementById("timer-days");
+  let timerInterval;
 
   const getTimeRemaining = () => {
     let dateStop = new Date(deadline).getTime();
     let dateNow = new Date().getTime();
     let timeRemaining = (dateStop - dateNow) / 1000;
+    // let days = Math.floor(timeRemaining / 3600 / 24);
 
-    let hours = Math.floor(timeRemaining / 60 / 60);
+    let hours = Math.floor(timeRemaining / 3600);
     let minutes = Math.floor((timeRemaining / 60) % 60);
     let seconds = Math.floor(timeRemaining % 60);
 
     return {
       timeRemaining,
+      // days,
       hours,
       minutes,
       seconds,
@@ -22,8 +28,8 @@ const timer = (deadline) => {
 
   const updateClock = () => {
     let getTime = getTimeRemaining();
-    console.log(getTime);
-
+    // timerDays.textContent =
+    //   getTime.days.toString().length === 1 ? "0" + getTime.days : getTime.days;
     timerHours.textContent =
       getTime.hours.toString().length === 1
         ? "0" + getTime.hours + "ч"
@@ -36,17 +42,21 @@ const timer = (deadline) => {
       getTime.seconds.toString().length === 1
         ? "0" + getTime.seconds + "сек"
         : getTime.seconds + "сек";
-
-    if (getTime.timeRemaining > 0) {
-      setTimeout(updateClock, 1000);
-    } else {
-      timerHours.textContent = "00";
-      timerMinutes.textContent = "00";
-      timerSeconds.textContent = "00";
-    }
+    return getTime.timeRemaining;
   };
 
-  updateClock();
+  // updateClock();
+
+  if (updateClock() > 0) {
+    timerInterval = setInterval(updateClock, 1000);
+  } else {
+    clearInterval(timerInterval);
+
+    // timerDays.textContent = "00";
+    timerHours.textContent = "00";
+    timerMinutes.textContent = "00";
+    timerSeconds.textContent = "00";
+  }
 };
 
 export default timer;
