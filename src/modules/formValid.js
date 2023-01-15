584c31f9414d07e8762af003249479a67f512abb
Viacheslav Kuminov <kuminov200220@gmail.com>
2023-01-15 18:01:00 +0700

    27 comp 12

--- src/modules/formValid.js
+++ src/modules/formValid.js
@@ -1,5 +1,35 @@
 "use strict";
 
+const inputValid = (item) => {
+  switch (true) {
+    case item.name === "user_name":
+      if (item.value.match(/[a-яA-я]{2}/gi)) {
+        item.classList.remove("error");
+      }
+      break;
+    case item.name === "user_email":
+      if (
+        item.value.match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+      ) {
+        item.classList.remove("error");
+      }
+      break;
+    case item.name === "user_phone":
+      if (
+        item.value.match(
+          /^((\+7|7|8)+\(?([0-9]){3}\)?)(\-?([0-9]){3})(\-?([0-9]){2}){2}$/g
+        )
+      ) {
+        item.classList.remove("error");
+      }
+      break;
+  }
+};
+
+//
+
 const formValid = () => {
   const formItemsText = document.querySelectorAll("form input[type=text]");
   const formItemsEmail = document.querySelectorAll("form input[type=email]");
@@ -12,6 +42,7 @@ const formValid = () => {
   formItemsText.forEach((formItemText) => {
     formItemText.addEventListener("input", () => {
       formItemText.value = formItemText.value.replace(/[^а-яА-Я\s\-]+/g, "");
+      if (formItemText.classList.contains("error")) inputValid(formItemText);
     });
   });
 
@@ -30,12 +61,14 @@ const formValid = () => {
         /[^a-zA-Z\d\@\-\_\.\!\~\*\']+/g,
         ""
       );
+      if (formItemEmail.classList.contains("error")) inputValid(formItemEmail);
     });
   });
 
   formItemsTel.forEach((formItemTel) => {
     formItemTel.addEventListener("input", () => {
       formItemTel.value = formItemTel.value.replace(/[^\d\\(\\)\-\+]+/g, "");
+      if (formItemTel.classList.contains("error")) inputValid(formItemTel);
     });
   });
 
--- src/modules/validator.js
+++ src/modules/validator.js
@@ -1,55 +1,39 @@
 "use strict";
 
+"use strict";
+
 const validator = (items) => {
   let flag = true;
+
   items.forEach((item) => {
-    switch (true) {
-      case item.name === "user_name":
-        if (!item.value.match(/[a-яA-я]{2}/gi)) {
-          flag = false;
-          item.classList.add("error");
-        }
-        if (item.value.match(/[a-яA-я]{2}/gi)) {
-          item.classList.remove("error");
-        }
-        break;
-        break;
-      case item.name === "user_email":
+    item.classList.remove("error");
+
+    switch (item.getAttribute("name")) {
+      case "user_name":
+        if (!item.value.match(/[a-яA-я]{2}/gi)) break;
+        return;
+      case "user_email":
         if (
           !item.value.match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
           )
-        ) {
-          flag = false;
-          item.classList.add("error");
-        }
-        if (
-          item.value.match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          )
-        ) {
-          item.classList.remove("error");
-        }
-        break;
-      case item.name === "user_phone":
+        )
+          break;
+        return;
+      case "user_phone":
         if (
           !item.value.match(
             /^((\+7|7|8)+\(?([0-9]){3}\)?)(\-?([0-9]){3})(\-?([0-9]){2}){2}$/g
           )
-        ) {
-          flag = false;
-          item.classList.add("error");
-        }
-        if (
-          item.value.match(
-            /^((\+7|7|8)+\(?([0-9]){3}\)?)(\-?([0-9]){3})(\-?([0-9]){2}){2}$/g
-          )
-        ) {
-          item.classList.remove("error");
-        }
-        break;
+        )
+          break;
+        return;
     }
+
+    item.classList.add("error");
+    flag = false;
   });
+
   return flag;
 };
 
